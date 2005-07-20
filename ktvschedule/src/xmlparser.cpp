56c97c3e7aa849f2d3e9b4813c7f61ee9feff36e
abeverat <abeverat>
2005-07-20 19:01:28 +0000

    New class for XML parsing.

---
+++ ktvschedule/src/xmlparser.cpp
@@ -0,0 +1,11 @@
+//
+// C++ Implementation: xmlparser
+//
+// Description: 
+//
+//
+// Author: Alexios Beveratos <alexios.beveratos@gmail.com>, (C) 2005
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
